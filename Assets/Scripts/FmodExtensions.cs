74411c9f7dff62322c11015fd80ff643e8d5cf30
poetahto <walls.dk@gmail.com>
2023-12-09 12:35:53 -0800

    better console debugging, fixed audio bugs

---
+++ Assets/Scripts/FmodExtensions.cs
@@ -0,0 +1,13 @@
+﻿using FMOD.Studio;
+namespace DefaultNamespace
+{
+    public static class FmodExtensions
+    {
+        public static string GetPath(this EventInstance instance)
+        {
+            instance.getDescription(out EventDescription desc);
+            desc.getPath(out string path);
+            return path;
+        }
+    }
+}
--- Assets/Scripts/Ltg8AudioSystem.cs
+++ Assets/Scripts/Ltg8AudioSystem.cs
@@ -1,16 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using DefaultNamespace;
 using FMOD.Studio;
+using FMODUnity;
+using poetools.Console.Commands;
 using UnityEngine;
+using STOP_MODE = FMOD.Studio.STOP_MODE;
 
-public class Ltg8AudioSystem : MonoBehaviour
+public class Ltg8AudioSystem : MonoBehaviour, IConsoleDebugInfo
 {
     /// <summary>
     /// A collection of FMOD instances that should persistent between scenes.
     /// </summary>
     public readonly Dictionary<string, EventInstance> PersistentAudio = new Dictionary<string, EventInstance>();
 
-    private readonly AnimatedInstance[] animatedInstances = new AnimatedInstance[50];
-    private int currentlyAnimatingInstances;
+    private readonly AnimatedInstance[] _animatedInstances = new AnimatedInstance[50];
+    private int _currentlyAnimatingInstances;
 
     /// <summary>
     /// Automatically changes an instances volume over time.
@@ -23,57 +28,61 @@ public class Ltg8AudioSystem : MonoBehaviour
     public void AnimateVolume(EventInstance instance, float target, float duration)
     {
         // Check if this instance is already being animated, and cancel it if so.
-        if (currentlyAnimatingInstances > 0)
+        if (_currentlyAnimatingInstances > 0)
         {
-            for (int i = 0; i < animatedInstances.Length; ++i)
+            for (int i = 0; i < _animatedInstances.Length; ++i)
             {
-                if (animatedInstances[i].IsValid && animatedInstances[i].Instance.handle == instance.handle)
+                if (_animatedInstances[i].IsValid && _animatedInstances[i].Instance.handle == instance.handle)
                 {
-                    animatedInstances[i].IsValid = false;
-                    currentlyAnimatingInstances--;
+                    _animatedInstances[i].IsValid = false;
+                    _currentlyAnimatingInstances--;
                 }
             }
         }
 
         // Check for an available slot in the instance array, and set it up for animation.
-        for (int i = 0; i < animatedInstances.Length; ++i)
+        for (int i = 0; i < _animatedInstances.Length; ++i)
         {
-            if (!animatedInstances[i].IsValid)
+            if (!_animatedInstances[i].IsValid)
             {
-                instance.getVolume(out animatedInstances[i].Initial);
-                animatedInstances[i].IsValid = true;
-                animatedInstances[i].Instance = instance;
-                animatedInstances[i].Duration = duration;
-                animatedInstances[i].Target = target;
-                animatedInstances[i].Elapsed = 0;
-                currentlyAnimatingInstances++;
+                instance.getVolume(out _animatedInstances[i].Initial);
+                _animatedInstances[i].IsValid = true;
+                _animatedInstances[i].Instance = instance;
+                _animatedInstances[i].Duration = duration;
+                _animatedInstances[i].Target = target;
+                _animatedInstances[i].Elapsed = 0;
+                _currentlyAnimatingInstances++;
+                break;
             }
         }
     }
     
     private void Awake()
     {
-        for (int i = 0; i < animatedInstances.Length; ++i)
-            animatedInstances[i].IsValid = false;
+        for (int i = 0; i < _animatedInstances.Length; ++i)
+            _animatedInstances[i].IsValid = false;
     }
 
     private void Update()
     {
         // Set the volume for all animating instances.
-        if (currentlyAnimatingInstances > 0)
+        if (_currentlyAnimatingInstances > 0)
         {
-            for (int i = 0; i < animatedInstances.Length; ++i)
+            for (int i = 0; i < _animatedInstances.Length; ++i)
             {
-                if (animatedInstances[i].IsValid)
+                if (_animatedInstances[i].IsValid)
                 {
-                    animatedInstances[i].Elapsed += Time.deltaTime;
-                    float t = animatedInstances[i].Elapsed / animatedInstances[i].Duration;
-                    float volume = Mathf.Lerp(animatedInstances[i].Initial, animatedInstances[i].Target, t);
-                    animatedInstances[i].Instance.setVolume(volume);
+                    _animatedInstances[i].Elapsed += Time.deltaTime;
+                    float t = _animatedInstances[i].Elapsed / _animatedInstances[i].Duration;
+                    float volume = Mathf.Lerp(_animatedInstances[i].Initial, _animatedInstances[i].Target, t);
+                    _animatedInstances[i].Instance.setVolume(volume);
 
                     // If the animation is finished, we stop it.
                     if (t >= 1)
-                        animatedInstances[i].IsValid = false;
+                    {
+                        _animatedInstances[i].IsValid = false;
+                        _currentlyAnimatingInstances--;
+                    }
                 }
             }
         }
@@ -88,4 +97,78 @@ public class Ltg8AudioSystem : MonoBehaviour
         public float Target;
         public float Initial;
     }
+    
+#region DEBUG
+    
+    public string DebugName => name;
+    private string _debugEventPath;
+    private string _debugId;
+    private string _debugDuration;
+    private string _debugVolume;
+    private readonly List<string> _debugRemovalQueue = new List<string>();
+    
+    public void DrawDebugInfo()
+    {
+        GUILayout.Label($"Animating instances: {_currentlyAnimatingInstances}");
+        
+        foreach (AnimatedInstance instance in _animatedInstances)
+        {
+            if (instance.IsValid)
+                GUILayout.Label($"{instance.Instance.GetPath()} [{instance.Elapsed / instance.Duration}] -> {instance.Target}");
+        }
+        
+        GUILayout.Label($"Persistent Audio: {PersistentAudio.Count}");
+        
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Volume");
+        _debugVolume = GUILayout.TextField(_debugVolume);
+        GUILayout.EndHorizontal();
+        
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Duration");
+        _debugDuration = GUILayout.TextField(_debugDuration);
+        GUILayout.EndHorizontal();
+            
+        _debugRemovalQueue.Clear();
+
+        foreach (string id in PersistentAudio.Keys)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label(id);
+            if (GUILayout.Button("x"))
+            {
+                PersistentAudio[id].stop(STOP_MODE.ALLOWFADEOUT);
+                PersistentAudio[id].release();
+                _debugRemovalQueue.Add(id);
+            }
+            if (GUILayout.Button("Set Volume"))
+                AnimateVolume(PersistentAudio[id], float.Parse(_debugVolume), float.Parse(_debugDuration));
+            GUILayout.EndHorizontal();
+        }
+
+        foreach (string idToRemove in _debugRemovalQueue)
+            PersistentAudio.Remove(idToRemove);
+
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Path");
+        _debugEventPath = GUILayout.TextField(_debugEventPath);
+        GUILayout.EndHorizontal();
+        
+        GUILayout.BeginHorizontal();
+        GUILayout.Label("Id");
+        _debugId = GUILayout.TextField(_debugId);
+        GUILayout.EndHorizontal();
+
+        if (GUILayout.Button("Play") && !PersistentAudio.ContainsKey(_debugId))
+        {
+            EventInstance instance = RuntimeManager.CreateInstance(_debugEventPath);
+            instance.start();
+            PersistentAudio.Add(_debugId, instance);
+            _debugId = string.Empty;
+            _debugEventPath = string.Empty;
+        }
+    }
+    
+#endregion
+
 }
