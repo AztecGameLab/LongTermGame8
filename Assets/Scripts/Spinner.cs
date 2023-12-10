c2056b7e54b19e489bb8358e22a1ad5a1a332343
poetahto <walls.dk@gmail.com>
2023-12-10 11:54:39 -0800

    added toon shader

---
+++ Assets/Scripts/Spinner.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+using Random = UnityEngine.Random;
+namespace DefaultNamespace
+{
+    public class Spinner : MonoBehaviour
+    {
+        public float spinSpeed;
+        public Vector3 axis;
+
+        private void Update()
+        {
+            transform.Rotate(axis, spinSpeed * Time.deltaTime);
+        }
+    }
+}
--- CREDITS.md
+++ CREDITS.md
@@ -3,3 +3,5 @@ Leave attributions to stuff that you use online here!
 
 - Red chair: https://www.sharetextures.com/models/furniture/chair_20
 - Rubber duck: https://polyhaven.com/a/rubber_duck_toy
+- toon shader: https://docs.unity3d.com/Packages/com.unity.toonshader@0.6/manual/index.html
+-  
