9ab3e3b05d503e763f2f66ec7a6c23a1819bdd4f
JonnyTheKing-3 <jonathangrcrdrgz@gmail.com>
2023-12-26 14:03:03 -0800

    Finished the script for Pause Manu, but menu is weird. Talk to India...
    The script itself does the job it's supposed but the UI menu itself is kind of compressed or stretched so it looks weird. If you press the buttons, they do the functions. But we need to fix that part of the UI. In other words, both scipts work fine, but fix the UI doc.

--- Assets/UI/Scripts for UI/UIControllerPauseMenu.cs
+++ Assets/UI/Scripts for UI/UIControllerPauseMenu.cs
@@ -1,15 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
+using Debug = UnityEngine.Debug;
 
 public class UIControllerPauseMenu : MonoBehaviour
 {
     [SerializeField] private KeyCode PauseKey = KeyCode.P;
     [SerializeField] private bool PauseMenuOnScreen = false;
     [SerializeField] private string currentPI = null;
-    [SerializeField] private string currentDisplay;
     
     private VisualElement _PauseMenu;
     private VisualElement _PageIndicator;
@@ -22,16 +23,39 @@ public class UIControllerPauseMenu : MonoBehaviour
     private Button _ButtonSave;
     private Button _ButtonExit;
     
+    private VisualElement _ContactsMenu;
+    private VisualElement _MapMenu;
+    private VisualElement _CollectionMenu;
+    private VisualElement _LogMenu;
+    private VisualElement _SettingsMenu;
+    private VisualElement _SaveMenu;
+    private VisualElement _ExitMenu;
+
+    public enum CurrentDisplay
+    {
+        Contacts,
+        Map,
+        Collection,
+        Log,
+        Settings,
+        Save,
+        Exit
+    }
+
+    private CurrentDisplay currDisplay;
     // Start is called before the first frame update
     void Start()
     {
         // Getting root to reach the other elements of UI document
         var root = GetComponent<UIDocument>().rootVisualElement;
         
-        // Getting references
+        // Getting references for...
+        
+        // Pause Menu and Page Indicator...
         _PauseMenu = root.Q<VisualElement>("PauseMenu");
         _PageIndicator = root.Q<VisualElement>("PageIndicator");
         
+        // Buttons...
         _ButtonContacts = root.Q<Button>("ButtonContacts");
         _ButtonMap = root.Q<Button>("ButtonMap");
         _ButtonCollections = root.Q<Button>("ButtonCollection");
@@ -39,6 +63,15 @@ public class UIControllerPauseMenu : MonoBehaviour
         _ButtonSettings = root.Q<Button>("ButtonSettings");
         _ButtonSave = root.Q<Button>("ButtonSave");
         _ButtonExit = root.Q<Button>("ButtonExit");
+        
+        // and sub-menus
+        _ContactsMenu = root.Q<VisualElement>("ContactsMenu");
+        _MapMenu = root.Q<VisualElement>("MapMenu");
+        _CollectionMenu = root.Q<VisualElement>("CollectionMenu");
+        _LogMenu = root.Q<VisualElement>("LogMenu");
+        _SettingsMenu = root.Q<VisualElement>("SettingsMenu");
+        _SaveMenu = root.Q<VisualElement>("SaveMenu");
+        _ExitMenu = root.Q<VisualElement>("ExitMenu");
         
         // Call methods to move the PageIndicator depending on which button is clicked
         _ButtonContacts.RegisterCallback<ClickEvent>(PageIndicatorToContacts);
@@ -83,9 +116,13 @@ public class UIControllerPauseMenu : MonoBehaviour
         // Move PageIndicator
         _PageIndicator.AddToClassList("PageIndicatorAtContacts");
         
-        
-        // Turn on display for thing clicked
         // Turn off the previous display
+        TurnOffCurrentDisplay();
+        // Make Contacts the new current display
+        currDisplay = CurrentDisplay.Contacts;
+        
+        // Turn on new display
+        _ContactsMenu.style.display = DisplayStyle.Flex;
     }
     private void PageIndicatorToMap(ClickEvent evt)
     {
@@ -101,8 +138,13 @@ public class UIControllerPauseMenu : MonoBehaviour
         // Move PageIndicator
         _PageIndicator.AddToClassList("PageIndicatorAtMap");
         
-        // Turn on display for thing clicked
         // Turn off the previous display
+        TurnOffCurrentDisplay();
+        // Make Contacts the new current display
+        currDisplay = CurrentDisplay.Map;
+        
+        // Turn on new display
+        _MapMenu.style.display = DisplayStyle.Flex;
     }
     private void PageIndicatorToCollection(ClickEvent evt)
     {
@@ -118,8 +160,13 @@ public class UIControllerPauseMenu : MonoBehaviour
         // Move PageIndicator
         _PageIndicator.AddToClassList("PageIndicatorAtCollection");
         
-        // Turn on display for thing clicked
         // Turn off the previous display
+        TurnOffCurrentDisplay();
+        // Make Contacts the new current display
+        currDisplay = CurrentDisplay.Collection;
+        
+        // Turn on new display
+        _CollectionMenu.style.display = DisplayStyle.Flex;
     }
     private void PageIndicatorToQuestLog(ClickEvent evt)
     {
@@ -135,8 +182,13 @@ public class UIControllerPauseMenu : MonoBehaviour
         // Move PageIndicator
         _PageIndicator.AddToClassList("PageIndicatorAtLog");
         
-        // Turn on display for thing clicked
         // Turn off the previous display
+        TurnOffCurrentDisplay();
+        // Make Contacts the new current display
+        currDisplay = CurrentDisplay.Log;
+        
+        // Turn on new display
+        _LogMenu.style.display = DisplayStyle.Flex;
     }
     private void PageIndicatorToSettings(ClickEvent evt)
     {
@@ -152,8 +204,13 @@ public class UIControllerPauseMenu : MonoBehaviour
         // Move PageIndicator
         _PageIndicator.AddToClassList("PageIndicatorAtSettings");
         
-        // Turn on display for thing clicked
         // Turn off the previous display
+        TurnOffCurrentDisplay();
+        // Make Contacts the new current display
+        currDisplay = CurrentDisplay.Settings;
+        
+        // Turn on new display
+        _SettingsMenu.style.display = DisplayStyle.Flex;
     }
     private void PageIndicatorToSave(ClickEvent evt)
     {
@@ -169,8 +226,13 @@ public class UIControllerPauseMenu : MonoBehaviour
         // Move PageIndicator
         _PageIndicator.AddToClassList("PageIndicatorAtSave");
         
-        // Turn on display for thing clicked
         // Turn off the previous display
+        TurnOffCurrentDisplay();
+        // Make Contacts the new current display
+        currDisplay = CurrentDisplay.Save;
+        
+        // Turn on new display
+        _SaveMenu.style.display = DisplayStyle.Flex;
     }
     private void PageIndicatorToExit(ClickEvent evt)
     {
@@ -186,7 +248,53 @@ public class UIControllerPauseMenu : MonoBehaviour
         // Move PageIndicator
         _PageIndicator.AddToClassList("PageIndicatorAtExit");
         
-        // Turn on display for thing clicked
         // Turn off the previous display
+        TurnOffCurrentDisplay();
+        // Make Contacts the new current display
+        currDisplay = CurrentDisplay.Exit;
+        
+        // Turn on new display
+        _ExitMenu.style.display = DisplayStyle.Flex;
+    }
+
+    private void TurnOffCurrentDisplay()
+    {
+        // Turn off the current displayed menu
+        switch (currDisplay)
+        {
+            case CurrentDisplay.Contacts:
+                Debug.Log("Turn off Contacts menu");
+                _ContactsMenu.style.display = DisplayStyle.None;
+                break;
+            case CurrentDisplay.Map:
+                Debug.Log("Turn off Map menu");
+                _MapMenu.style.display = DisplayStyle.None;
+                break;
+            case CurrentDisplay.Collection:
+                Debug.Log("Turn off Collection menu");
+                _CollectionMenu.style.display = DisplayStyle.None;
+                break;
+            case CurrentDisplay.Log:
+                Debug.Log("Turn off Log menu");
+                _LogMenu.style.display = DisplayStyle.None;
+                break;
+            case CurrentDisplay.Settings:
+                Debug.Log("Turn off Settings menu");
+                _SettingsMenu.style.display = DisplayStyle.None;
+                break;
+            case CurrentDisplay.Save:
+                Debug.Log("Turn off Save menu");
+                _SaveMenu.style.display = DisplayStyle.None;
+                break;
+            case CurrentDisplay.Exit:
+                Debug.Log("Turn off Exit menu");
+                _ExitMenu.style.display = DisplayStyle.None;
+                break;
+            default:
+                // No menu is being displayed
+                Debug.Log("No current menu on display");
+                break;
+        }
     }
 }
+
