e7cebea0ae36a7e6008e4f18d3e293c706f2b01d
Mysti <mystievous@gmail.com>
2024-01-03 17:29:30 -0800

    implement Shrink In Place collection in main code

--- Assets/Scripts/Collectibles/PlacedCollectibleItem.cs
+++ Assets/Scripts/Collectibles/PlacedCollectibleItem.cs
@@ -1,4 +1,6 @@
+using System;
 using FMODUnity;
+using UnityEngine;
 
 namespace Collectibles
 {
@@ -6,15 +8,39 @@ namespace Collectibles
     {
         public InventoryCollectibleItem collectibleItem;
         public EventReference collectSound;
+        public float timeToShrink = 0.35f;
+        private float _elapsedShrinkTime;
+        private Vector3 _startScale;
+        private bool _collecting;
 
         public override void Interact(PlayerInteractController playerInteractController)
         {
             playerInteractController.Inventory.collectedItems.Add(collectibleItem);
             RuntimeManager.PlayOneShot(collectSound, transform.position);
-            gameObject.SetActive(false);
+            // gameObject.SetActive(false);
+            interactionEnabled = false;
+            _startScale = transform.localScale;
+            _collecting = true;
         }
-        
-        
+
+        private void Update()
+        {
+            if (!_collecting)
+                return;
+
+            Transform itemTransform = transform;
+
+            _elapsedShrinkTime = Math.Min(_elapsedShrinkTime + Time.deltaTime, timeToShrink);
+            
+            float ratio = _elapsedShrinkTime / timeToShrink;
+
+            transform.localScale = Vector3.Lerp(_startScale, Vector3.zero, ratio);
+            
+            if (_elapsedShrinkTime >= timeToShrink)
+            {
+                Destroy(gameObject);
+            }
+        }
         
     }
 }
