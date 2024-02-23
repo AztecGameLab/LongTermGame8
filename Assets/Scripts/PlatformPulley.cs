324b7577b028a38983821829bef3ac1eeed8ad72
Madison Bennett <114691834+madBennett@users.noreply.github.com>
2024-02-23 15:21:23 -0800

    Add Weight Factor
    -Add logic for adding weight on col enter and subtracting weight on col exit
    -Added additional reference to other platformpulley instance
    -Set reference to other in test scene

--- Assets/Scripts/PlatformPulley.cs
+++ Assets/Scripts/PlatformPulley.cs
@@ -8,34 +8,34 @@ namespace Ltg8
     {
         //public GameObject hitPlatformRef;
         public GameObject otherPlatformRef;
+        public PlatformPulley otherplatformPulleyRef;
 
-        public float movementSpeed = 1f;
+        public float movementSpeed = 0.1f;
         public float dampen = 1f;
 
         public float weightOn = 0f;
 
         void OnCollisionEnter(Collision col)
         {
-            float scalar1 = (col.impulse.magnitude / dampen) + weightOn;//add weight calc here
-            float scalar2 = (col.impulse.magnitude / dampen) - weightOn;//change do getting wieght on other
+            //Use the Impluse as the force moving the platforms down and account for the weights already on each platform
+            float scalar1 = (col.impulse.magnitude / dampen) + weightOn;
+            float scalar2 = (col.impulse.magnitude / dampen) - otherplatformPulleyRef.weightOn;
 
+            //Move both in respects to the impact and wieghts on each
             this.transform.position = Vector3.MoveTowards(this.transform.position, 
                 this.transform.position + Vector3.down * scalar1, movementSpeed * Time.deltaTime);
 
             otherPlatformRef.transform.position = Vector3.MoveTowards(otherPlatformRef.transform.position, 
                 otherPlatformRef.transform.position + Vector3.up * scalar2, movementSpeed * Time.deltaTime);
-        }
 
-        void OnCollisionStay(Collision col)
-        {
-            //add weight
-            //weightOn += col.rigidbody.mass;
-            Debug.Log("Weight on: " + weightOn);
+            //add to the current weight of this platform
+            weightOn += col.rigidbody.mass;
         }
 
         void OnCollisionExit(Collision col)
         {
             //remove weight
+            weightOn -= col.rigidbody.mass;
         }
     }
 }
