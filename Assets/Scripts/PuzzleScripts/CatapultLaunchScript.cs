d0d8d55ba411063d9a9be8526de9d9ff1fd829f4
Spectrum-Programmer <48571227+Spectrum-Programmer@users.noreply.github.com>
2024-02-23 00:35:33 -0800

    Catapult Launch Script Update
    The Launch Script is now located under Assets/Scripts/PuzzleScripts
    
    This is not a new update but I also wanted to mention that the testing for this script was done in Assets/Scene/DevAssets/Physics-Testing
    
    I have added a few things to the script, such as
    - The script can now be added into any catapult model. The catapult variable will update to the catapult automatically, and as long as it has a "Spoon" (must be named that), the script will find it and utilize its rotation for the launch angle.
    - In my efforts to add the above change, I added a FindFirstChild function to search the tree of dependencies, which could be used for other purposes (This function is not optimized, currently it may only work for a tree with exclusively GameObjects. It also doesn't utilize any fast Traversal operations.
    - The launch trajectory no longer relies solely on the x-y axis. Whichever direction the catapult is facing, the object will launch.
    - Added a "Set Projectile" function, which is currently not in use.

---
+++ Assets/Scripts/PuzzleScripts/CatapultLaunchScript.cs
@@ -0,0 +1,79 @@
+using System;
+using UnityEngine;
+
+public class CatapultLaunchScript : MonoBehaviour
+{
+
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private Rigidbody projectile_rb;
+    private GameObject catapult;
+    private GameObject catapult_beam;
+
+    [SerializeField] private int distance;
+    [SerializeField] private float land_vertical_y;
+    private float launch_angle;
+    
+    private float vertical_difference;
+    private bool child_found;
+    
+    void Start()
+    {
+        catapult = transform.gameObject;
+        catapult_beam = FindFirstChild(catapult, "Spoon").gameObject;
+        launch_angle = catapult_beam.transform.rotation.eulerAngles.x * Mathf.Deg2Rad;
+        vertical_difference = projectile_rb.transform.position.y - land_vertical_y;
+        LaunchProjectile();
+    }
+
+    private void SetProjectile(GameObject projectile)
+    {
+        this.projectile = projectile;
+        projectile_rb = projectile.GetComponent<Rigidbody>();
+    }
+    
+    private void LaunchProjectile()
+    {
+        float velocity_h = (float)(CalculateVelocity() * Math.Cos(launch_angle));
+        float velocity_v = (float)(CalculateVelocity() * Math.Sin(launch_angle));
+        float velocity_x = velocity_h * catapult.transform.forward.x;
+        float velocity_z = velocity_h * catapult.transform.forward.z;
+        projectile_rb.velocity = new Vector3(velocity_x, velocity_v, velocity_z);
+    }
+
+    private float CalculateVelocity() {
+
+        double numerator = Math.Sqrt(472.0380705f) * (distance+1);
+
+        double denominator = Math.Sqrt(96.2361f) * Math.Sqrt(
+            Math.Cos(launch_angle) * (
+                (distance+1) * Math.Sin(launch_angle) +
+                vertical_difference * Math.Cos(launch_angle)
+            )
+        );
+        return (float)(numerator / denominator);
+    }
+
+    private Transform FindFirstChild(GameObject parent, string child_name)
+    {
+        if (child_found) { child_found = false; return null; }
+        Transform child = parent.transform.Find(child_name);
+        if (child)
+        { child_found = true; return child; }
+        if(parent.transform.childCount > 0)
+        {
+            for (int i = 0; i < parent.transform.childCount; i++)
+            {
+                child = FindFirstChild(parent.transform.GetChild(i).gameObject, child_name);
+                if (child)
+                {
+                    return child;
+                }
+            }
+        }
+        return null;
+    }
+
+    void Update()
+    {
+    }
+}
--- Assets/Scripts/PuzzleScripts/CatapultRotationScript.cs
+++ Assets/Scripts/PuzzleScripts/CatapultRotationScript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Launch : MonoBehaviour
+public class CatapultRotationScript : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
