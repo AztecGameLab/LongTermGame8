75f233138ba555dac0e7b18c23b79cc44494d9af
RasJazz <jasmine.rasmussen.a@gmail.com>
2024-02-26 17:43:58 -0800

    Added placeholder animations and logic
    - Animator controller for animations
    - Created idle and walk placeholder animations
    - Implemented logic for which animations play based on player movement state
    - Created Animation test scene for testing
    - Added Sigmund.obj on Player prefab to test model

---
+++ Assets/Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using DefaultNamespace;
+using poetools.Core.Abstraction;
+using UnityEngine;
+
+namespace Ltg8.Player
+{
+    public class PlayerAnimation : MonoBehaviour
+    {
+        private static readonly int IsIdle = Animator.StringToHash("isIdle");
+        
+        [SerializeField] private PhysicsComponent player;
+        [SerializeField] private Animator animatorController;
+        private float _playerInitialMagnitude;
+        private bool _isIdle;
+
+        private void Start()
+        {
+            animatorController.SetBool(IsIdle, true);
+            // Debug.Log(player.Velocity.magnitude);
+            // Initial magnitude of Sigmund for logic checks (temp const due to bugs)
+            _playerInitialMagnitude = player.Velocity.magnitude;
+        }
+
+        private void Update()
+        {
+            CheckPlayerIdleState(player.Velocity.magnitude);
+        }
+
+        private void CheckPlayerIdleState(float currentMagnitude)
+        {
+            // TO DO: Pass in layer to ChangeAnimationState
+            Debug.Log(currentMagnitude);
+            // If Sigmund isn't moving, plays animation
+            if (currentMagnitude <= _playerInitialMagnitude)
+            {
+                Debug.Log("Sigmund isn't moving");
+                ChangeAnimationState(true);
+            }
+            else
+            {
+                ChangeAnimationState(false);
+            }
+        }
+
+        public void CreateAnimationList(Animator test)
+        {
+            
+        }
+
+        private void ChangeAnimationState(bool newState)
+        {
+            animatorController.SetBool(IsIdle, newState);
+        }
+
+        private void OnGUI()
+        {
+            // Debug UI code
+            // static cl
+            GUILayout.Label("Hello World");
+        }
+    }
+}
