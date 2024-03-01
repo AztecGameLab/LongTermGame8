79e100a01a2ce709be0563b659801454b1e77109
Daniel Walls <walls.dk@gmail.com>
2024-03-01 11:41:47 -0800

    walkie and character texture

---
+++ Assets/Scripts/Animation/ImageFlipbookView.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Ltg8
+{
+    public class ImageFlipbookView : FlipBookView
+    {
+        [SerializeField] private Image image;
+
+        public override void DisplayImage(Sprite sprite)
+        {
+            image.color = sprite == null ? Color.clear : Color.white;
+            image.sprite = sprite;
+        }
+    }
+}
