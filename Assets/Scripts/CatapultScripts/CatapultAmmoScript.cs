7bc61c43061017457e709c281e813b95f8c576e8
Spectrum_Programmer <justin.adams1015@gmail.com>
2024-03-02 00:28:10 -0800

    Catapult Launching Complete
    Finishing some of the finishing touches for the Catapult
    - The Catapult can now launch items from the players inventory
    - The Catapult can now effectively launch the player themselves
    - The player can unload the catapult via the same method they would use to enter it (Pressing 'E' on it)
    - Anything launched via the catapult will immediately come to an abrupt stop upon reaching the ground to prevent unpredictable landing scenarios.
    
    Also started working on some Audio stuff but decided to just come back to that later.

--- Assets/Scripts/CatapultScripts/CatapultAmmoScript.cs
+++ Assets/Scripts/CatapultScripts/CatapultAmmoScript.cs
@@ -1,4 +1,6 @@
-using Cysharp.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+using Catapult;
 using Ltg8.Inventory;
 using Ltg8.Player;
 using UnityEngine;
@@ -8,9 +10,13 @@ public class CatapultAmmoScript : MonoBehaviour
     public Transform loadPosition;
     public Transform unloadPosition;
 
-    private bool _loaded;
+    [SerializeField] private bool _loaded;
 
-    private GameObject _loadedItem;
+    public GameObject _loadedItem;
+
+    [SerializeField] private GameObject launchBasketProx;
+    [SerializeField] private GameObject catapult;
+    [SerializeField] private GameObject catapult_basket;
 
     private void OnGUI()
     {
@@ -30,7 +36,14 @@ public class CatapultAmmoScript : MonoBehaviour
     public void ObjectLoaded(GameObject enteredObject)
     {
         if (_loaded && _loadedItem != null) return;
-        
+        enteredObject.GetComponent<Rigidbody>().useGravity = false;
+        enteredObject.GetComponent<Rigidbody>().angularDrag = 0;
+        enteredObject.AddComponent<SphereCollider>();
+        enteredObject.AddComponent<TouchGrass>();
+        enteredObject.GetComponent<SphereCollider>().isTrigger = true;
+        enteredObject.GetComponent<SphereCollider>().enabled = false;
+        enteredObject.GetComponent<SphereCollider>().radius = 0.65f;
+        catapult.GetComponent<CatapultLaunchScript>().SetProjectile(enteredObject);
         _loaded = true;
         _loadedItem = enteredObject;
     }
@@ -38,20 +51,44 @@ public class CatapultAmmoScript : MonoBehaviour
     public void ObjectUnloaded(GameObject exitedObject)
     {
         if (_loadedItem == null || exitedObject != _loadedItem) return;
-        
+        if (!catapult.GetComponent<CatapultLaunchScript>().launched_object == _loadedItem)
+        {
+            StartCoroutine(RemoveItem(_loadedItem, 0.1f));
+        }
         _loaded = false;
         _loadedItem = null;
     }
 
     public void PlacePlayer(GameObject player)
     {
-        if (_loaded) return;
+        if (_loaded) {Unload(); return;}
 
         if (player.TryGetComponent(out CharacterController playerController))
         {
-            playerController.Move(loadPosition.position - player.transform.position);
+            //playerController.Move(loadPosition.position - player.transform.position);
+            player.GetComponent<CharacterController>().enabled = false;
+            player.transform.position = loadPosition.position;
+            StartCoroutine(DelayProx(0.5f,player));
+            _loaded = true;
+            _loadedItem = player;
+            catapult.GetComponent<CatapultLaunchScript>().SetProjectile(player);
         }
-        // player.transform.position = loadPosition.position;
+    }
+
+    public void UnplacePlayer(GameObject player)
+    {
+        if (player.TryGetComponent(out CharacterController playerController))
+        {
+            if (playerController.enabled)
+            {
+                launchBasketProx.SetActive(false);
+                _loaded = false;
+                _loadedItem = null;
+                catapult.GetComponent<CatapultLaunchScript>().SetProjectile(null);
+                Debug.Log("Unplace Player");
+                catapult_basket.GetComponent<FixedJoint>().connectedBody = null;
+            }
+        }
     }
 
     public async void PlaceItem(ItemData item)
@@ -71,17 +108,35 @@ public class CatapultAmmoScript : MonoBehaviour
         }
         else
         {
-            Destroy(_loadedItem);
+            _loadedItem.transform.position += new Vector3(0,-3,0);
+            _loadedItem.SetActive(false);
         }
+    }
 
-        _loaded = false;
-        _loadedItem = null;
+    private IEnumerator<WaitForSeconds> RemoveItem(GameObject item, float time)
+    {
+        yield return new WaitForSeconds(time);
+        if (item && !(item.TryGetComponent(out CharacterController characterController)))
+        {
+            Destroy(item);
+            Debug.Log("After Destroy");
+            catapult_basket.GetComponent<FixedJoint>().connectedBody = null;
+            catapult.GetComponent<CatapultLaunchScript>().SetProjectile(null);
+        }
     }
 
     // Also should be called for firing the catapult
     public void Reset()
     {
-        _loadedItem = null;
-        _loaded = false;
+        /*_loadedItem = null;
+        _loaded = false;*/
+    }
+    
+    private IEnumerator<WaitForSeconds> DelayProx(float time, GameObject player)
+    {
+        yield return new WaitForSeconds(time);
+        launchBasketProx.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        player.GetComponent<CharacterController>().enabled = true;
     }
 }
