32658fb091f816a193c59f5500afec57f84687a7
Daniel Walls <walls.dk@gmail.com>
2024-03-06 16:35:45 -0800

    changed to new item target for bucket

---
+++ Assets/Scripts/Inventory/Targets/IncludeItemTarget.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ltg8.Inventory
+{
+    public class IncludeItemTarget : ItemTarget
+    {
+        [SerializeField]
+        private List<string> allowedItems;
+        
+        public override bool CanReceiveItem(ItemData data)
+        {
+            foreach (string allowedItemId in allowedItems)
+                if (data.guid == allowedItemId) return true;
+
+            return false;
+        }
+    }
+}
