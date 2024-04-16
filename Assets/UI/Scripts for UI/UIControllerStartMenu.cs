042c143ca0f0cf90a462628b9667e0d0c70e32a9
JonnyTheKing-3 <jonathangrcrdrgz@gmail.com>
2024-04-15 19:56:06 -0700

    unity background stuff

--- Assets/UI/Scripts for UI/UIControllerStartMenu.cs
+++ Assets/UI/Scripts for UI/UIControllerStartMenu.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -22,6 +23,7 @@ public class UIControllerStartMenu : MonoBehaviour
         _ButtonContainer = root.Q<VisualElement>("ButtonContainer");
         _ScreenSpace = root.Q<VisualElement>("ScreenSpace");
         _ButtonStart = root.Q<Button>("ButtonStart");
+        
         
         _ButtonStart.RegisterCallback<ClickEvent>(WhenStartIsPressed);
     }
