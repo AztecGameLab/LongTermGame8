7bf2b35fb10fc821ddc6ee3e9b5d333899beaa4b
Mysti <mystievous@gmail.com>
2024-07-02 01:33:25 -0700

    make pause menu hide dialogue boxes

--- Assets/Scripts/GameState/OverworldState/PausedState.cs
+++ Assets/Scripts/GameState/OverworldState/PausedState.cs
@@ -10,37 +10,53 @@ namespace Ltg8
         public OverworldGameState OverworldState { get; set; }
 
         private UIControllerPauseMenu _pauseMenu;
+        private TextBoxView[] _dialogues;
 
-        public async UniTask OnEnter()
+        public UniTask OnEnter()
         {
             _pauseMenu = Object.FindAnyObjectByType<UIControllerPauseMenu>();
             _pauseMenu.onClose.AddListener(HandleOnClose);
             _pauseMenu.Open();
-            
-            Cursor.visible = true; 
+
+            _dialogues = Object.FindObjectsByType<TextBoxView>(FindObjectsSortMode.None);
+            foreach (var dialogue in _dialogues)
+            {
+                dialogue.gameObject.SetActive(false);
+            }
+
+            Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
-            
+
             Ltg8.Controls.GameplayCommon.Pause.performed += HandleClosePause;
+            return UniTask.CompletedTask;
         }
-        
-        public async UniTask OnExit()
+
+        public UniTask OnExit()
         {
-            _pauseMenu.Close();
-            
             _pauseMenu.onClose.RemoveListener(HandleOnClose);
+            _pauseMenu.Close();
+
+            foreach (var dialogue in _dialogues)
+            {
+                dialogue.gameObject.SetActive(true);
+            }
+
             Ltg8.Controls.GameplayCommon.Pause.performed -= HandleClosePause;
+            return UniTask.CompletedTask;
         }
-        
+
         private void HandleOnClose()
         {
             OverworldState.StateMachine.TransitionTo(OverworldState.ExploringState).Forget();
         }
-        
+
         private void HandleClosePause(InputAction.CallbackContext context)
         {
             _pauseMenu.Close();
         }
 
-        public void OnUpdate() {}
+        public void OnUpdate()
+        {
+        }
     }
 }
